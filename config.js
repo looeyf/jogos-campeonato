f06755dbadd1077910a1af216cf620a054efdd90
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2021-06-23 20:51:22 -0300

    Variaveis de ambiente

---
+++ config.js
@@ -0,0 +1,3 @@
+import { LIVE_KEY, TEST_KEY } from 'react-native-dotenv';
+
+export default { LIVE_KEY, TEST_KEY }
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import config from '../../config';
 
 const api = axios.create({
   baseURL: 'https://api.api-futebol.com.br/v1/campeonatos',
 });
 
 api.defaults.headers.common['Authorization'] =
-  'Bearer live_a02051125026def9af7b3f670983e5';
-// 'Bearer test_1554c9a480ffc81bd0ed0200f4740f';
+  `Bearer ${config.LIVE_KEY}`;
+// 'Bearer ${config.TEST_KEY}';
 
 export default api;
